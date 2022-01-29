00077fd993978cd31a599cc1da900e77fdf78bb1
Danyil <demyanyuk76@gmail.com>
2022-01-29 17:48:51 +0200

    some folders changes

--- .history/src/index_20220129174825.js
+++ .history/src/index_20220129174825.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.module.scss";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import PageRouting from "routing/Routing";
 
 ReactDOM.render(
   <React.StrictMode>
