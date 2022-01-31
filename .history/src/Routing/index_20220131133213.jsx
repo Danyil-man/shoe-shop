4290351a1ffe03762e598cca00a8cea715d61631
Danyil <demyanyuk76@gmail.com>
2022-01-31 13:54:31 +0200

    some changes in json

---
+++ .history/src/Routing/index_20220131133213.jsx
@@ -0,0 +1,22 @@
+import Drawer from "components/Drawer";
+import Header from "components/Header";
+import Favorites from "pages/favorites/Favorites";
+import Home from "pages/home/Home";
+import Profile from "pages/profile/Profile";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const PageRouting = () => {
+  return (
+    <BrowserRouter basename="/shoe-shop">
+      <Drawer />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/favorite" component={Favorites} />
+        <Route exact path="/profile" component={Profile} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+export default PageRouting;
